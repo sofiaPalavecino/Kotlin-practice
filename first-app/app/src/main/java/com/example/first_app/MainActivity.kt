5cb9c0e3ac4e91dc1b863c285ee93e52944b95cb
Sofi <sofi@chofineta-2.0>
2022-09-23 00:47:45 -0300

    Added super cool functionality B)

--- first-app/app/src/main/java/com/example/first_app/MainActivity.kt
+++ first-app/app/src/main/java/com/example/first_app/MainActivity.kt
@@ -3,11 +3,24 @@ package com.example.first_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.dice_button)
+        rollButton.setOnClickListener{
+            Toast.makeText(this, "diced B)", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val diceValue: TextView = findViewById(R.id.dice_value)
+        val randomInt = Random().nextInt(6)+1
+        diceValue.text = randomInt.toString()
     }
 }
